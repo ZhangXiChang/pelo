fab31736e70ff94dd1cd3770daf1b405e075da52
ZhangXiChang <yxrbeigua@outlook.com>
2024-01-09 12:54:06 +0800

    异步信道通信

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,54 @@
+use std::time;
+
 use anyhow::{Ok, Result};
-use crossbeam_channel::{unbounded, Receiver, Sender};
+use crossbeam_channel::unbounded;
+use crossterm::event;
 
-#[allow(unused)]
-trait MessageHandling {
-    fn message_handling(&mut self, msg: (Sender<Message>, Receiver<Message>)) -> Result<()> {
-        Ok(())
-    }
-}
-
-#[allow(unused)]
 enum Message {
     Quit,
 }
 
-struct App {
-    message: (Sender<Message>, Receiver<Message>),
-    systems: Vec<Box<dyn MessageHandling>>,
-}
-impl App {
-    fn new() -> Self {
-        Self {
-            message: unbounded(),
-            systems: vec![],
-        }
-    }
-    fn run(&mut self) -> Result<()> {
-        for system in &mut self.systems {
-            system.message_handling(self.message.clone())?;
-        }
-        self.message_handling(self.message.clone())?;
-        Ok(())
-    }
-}
-impl MessageHandling for App {
-    fn message_handling(&mut self, msg: (Sender<Message>, Receiver<Message>)) -> Result<()> {
-        match msg.1.recv()? {
-            Message::Quit => drop(msg),
-        }
-        Ok(())
-    }
-}
-
 #[tokio::main]
 async fn main() -> Result<()> {
-    App::new().run()?;
+    let (s_msg, r_msg) = unbounded::<Message>();
+    let async_loop_r_msg = r_msg.clone();
+    tokio::spawn(async move {
+        loop {
+            if event::poll(time::Duration::from_millis(0))? {
+                match event::read()? {
+                    event::Event::Key(_) => s_msg.send(Message::Quit)?,
+                    _ => (),
+                }
+            }
+            match async_loop_r_msg.try_recv() {
+                core::result::Result::Ok(msg) => match msg {
+                    Message::Quit => {
+                        drop(async_loop_r_msg);
+                        break;
+                    }
+                },
+                Err(err) => match err {
+                    crossbeam_channel::TryRecvError::Empty => (),
+                    crossbeam_channel::TryRecvError::Disconnected => break,
+                },
+            }
+        }
+        Ok(())
+    });
+    let main_loop_r_msg = r_msg.clone();
+    loop {
+        match main_loop_r_msg.try_recv() {
+            core::result::Result::Ok(msg) => match msg {
+                Message::Quit => {
+                    drop(main_loop_r_msg);
+                    break;
+                }
+            },
+            Err(err) => match err {
+                crossbeam_channel::TryRecvError::Empty => (),
+                crossbeam_channel::TryRecvError::Disconnected => break,
+            },
+        }
+    }
     Ok(())
 }
